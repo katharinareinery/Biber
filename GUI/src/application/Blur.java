44a84707005f7ee9637daac1816d155e4919aff1
Chulien <julien.stein@hs-bochum.de>
2018-05-29 16:53:46 +0200

    blur yeah

--- GUI/src/application/Blur.java
+++ GUI/src/application/Blur.java
@@ -4,32 +4,37 @@ import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
-public class Blur{
+public class Blur extends ImageMan{
 	int DELAY_CAPTION = 1500;
 	int DELAY_BLUR = 100;
 	int MAX_KERNEL_LENGHT = 31;
-	Mat dst = new Mat();
-	Mat src = new Mat();
+	private Mat dst = new Mat();
 	
-	public Mat imageMan(Mat src) {
-		this.src=src;		
+	public Mat imageMan(Mat src) {		
 			for(int i = 1; i < MAX_KERNEL_LENGHT; i = i + 2) {
 				Imgproc.GaussianBlur(src, dst, new Size(i , i), 0, 0);
-				displayDst(DELAY_BLUR);
 			}
 		return dst;
 	}
-
-
-	private boolean displayCaption(String caption) {
-		dst = Mat.zeros(src.size(), src.type());
-		//Imgproc.putText(dst, caption, new Point(src.cols()/4, src.rows()/2, Core.FONT_HERSHEY_COMPLEX, 1, new Scalar(255,255,255));
-		return false;
+	
+	public Mat medianBlur(Mat src) {
+		for(int i = 1; i<MAX_KERNEL_LENGHT; i=i+2) {
+			Imgproc.medianBlur(src, dst, i);
+		}
+		return dst;
 	}
 	
-	private void displayDst(int delay) {
-		// TODO Auto-generated method stub
-		
+	public Mat homogenBlur(Mat src) {
+		for(int i = 1; i < MAX_KERNEL_LENGHT; i=i+2) {
+			Imgproc.blur(src, dst, new Size( i, i ), new Point(-1,-1));
+		}
+		return dst;
 	}
 	
+	public Mat biliteralBlur(Mat src) {
+		for(int i = 1; i < MAX_KERNEL_LENGHT; i=i+2) {
+			Imgproc.bilateralFilter(src, dst, i, i*2, i/2);
+		}
+		return dst;
+	}
 }
