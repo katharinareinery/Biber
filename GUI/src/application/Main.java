cadd521138da80e98ab5a65554a01997a870b4db
Katharina Reinery <katharinareinery@users.noreply.github.com>
2018-09-03 14:36:51 +0200

    Tooltip hinzugefügt und Mainfenster an Bildschirmgröße angepasst.

--- GUI/src/application/Main.java
+++ GUI/src/application/Main.java
@@ -1,12 +1,17 @@
 package application;
 	
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
 import org.opencv.core.Core;
 
 import javafx.application.Application;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 
 
 public class Main extends Application {
@@ -14,18 +19,20 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception{
 		
-			//BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("GUI.fxml"));test
+			//BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("GUI.fxml"));
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("LGUI.fxml"));
 			Parent root = loader.load();
 			FXController controller = (FXController)loader.getController();
 			controller.init(primaryStage);
-			
-			Scene scene = new Scene(root,950,600);
+							
+			Scene scene = new Scene(root,950,650);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("Biber");
 			primaryStage.setScene(scene);
+			//Fullscreenmodus
+			//primaryStage.setFullScreen(true);
+			primaryStage.setMaximized(true);
 			primaryStage.show();
-
 	}
 
 	
