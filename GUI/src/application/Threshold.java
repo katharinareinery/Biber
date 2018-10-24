2bc5ee5f38a2d4c9b8e7bfe911c27d1e1c0903a4
Lukerstroopers <lukerstroopers@gmail.com>
2018-10-24 14:51:22 +0200

    asdf

--- GUI/src/application/Threshold.java
+++ GUI/src/application/Threshold.java
@@ -56,15 +56,26 @@ public class Threshold extends ImageMan{
 		}
 		//thresholding method
 		public Mat binarisieren(int t, Mat src) {
-			Imgproc.cvtColor(src, bwsrc, Imgproc.COLOR_BGR2GRAY);
-			dst = new Mat(bwsrc.size(),CvType.CV_8U);
-			for(int i = 0; i<bwsrc.rows();i++) {
-				for(int j = 0; j <bwsrc.cols();j++) {
-//					System.out.print("row: "+i+"\t");
-//					System.out.println("col: "+j);
-					if(bwsrc.get(i,j)[0]>=t) {
-						dst.put(i, j, 255);
-					}else dst.put(i, j, 0);
+			if(src.channels() > 1) {
+				Imgproc.cvtColor(src, bwsrc, Imgproc.COLOR_BGR2GRAY);
+				dst = new Mat(bwsrc.size(),CvType.CV_8U);
+				for(int i = 0; i<bwsrc.rows();i++) {
+					for(int j = 0; j <bwsrc.cols();j++) {
+//						System.out.print("row: "+i+"\t");
+//						System.out.println("col: "+j);
+						if(bwsrc.get(i,j)[0]>=t) {
+							dst.put(i, j, 255);
+						}else dst.put(i, j, 0);
+					}
+				}
+			}else {
+				dst = new Mat(src.size(),CvType.CV_8U);
+				for(int i = 0; i<src.rows();i++) {
+					for(int j = 0; j <src.cols();j++) {
+						if(src.get(i,j)[0]>=t) {
+							dst.put(i, j, 255);
+						}else dst.put(i, j, 0);
+					}
 				}
 			}
 			return dst;
